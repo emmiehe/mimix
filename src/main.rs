b37d19787c758be2be781ef954a48c06c2ff8ea7
Emmie He <yimin_he@brown.edu>
2022-08-14 21:38:12 -0700

    followed VGA text mode: https://os.phil-opp.com/vga-text-mode/

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,33 @@
 // overwrite the entry point
 #![no_main]
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("OH NO: {}", info);
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
+// static HELLO: &[u8] = b"Hello World!";
 
 // overwriting default entry point
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    // let vga_buffer = 0xb8000 as *mut u8;
+    //
+    // for (i, &byte) in HELLO.iter().enumerate() {
+    //     unsafe {
+    //         *vga_buffer.offset(i as isize * 2) = byte;
+    //         *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+    //     }
+    // }
+    // vga_buffer::print_something();
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("Hello {}", "World");
+    // panic!("here is my panic");
 
     loop {}
 }
